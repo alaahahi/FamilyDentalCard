cca81a1199024713cf7cf6caacc9e31eaff450d3
alaahahi <alaa.hahe@gmail.com>
2023-10-21 14:15:44 +0300

    adds

---
+++ resources/views/doctor/Celebrity-Smile.php
@@ -0,0 +1,177 @@
+@extends('layouts.content')
+@section('content')
+  <div class="page-heading">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 offset-lg-2 header-text">
+          <h2><em>Celebrity Smile</em> Center</h2>
+          <p> B.D.S. M.Sc. in Prosthodontics </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="top-categories">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg col-sm-4">
+          <div class="item">
+            <div class="icon">
+              <img src="assets/images/icon-09.png" alt="">
+            </div>
+            <h4>فحص مجاني</h4>
+            <span>الخدمات المجانية</span>
+            <span class="counter">نسبة الخصم 100</span>
+          </div>
+        </div>
+
+
+
+        <div class="col-lg col-sm-4">
+          <div class="item">
+            <div class="icon">
+              <img src="assets/images/icon-09.png" alt="">
+            </div>
+            <h4> غسل و بولش أسنان </h4>
+            <span>الخدمات المجانية</span>
+            <span class="counter">لشخص واحد للعائلة</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <section class="featured-contests">
+    <div class="container">
+      <!-- <div class="row">
+        <div class="col-lg-12">
+          <div class="section-heading text-center">
+            <h6>قائمة الأسعار</h6>
+            <h4>احصل على تخفيضات <em>و خدمات مجانية</em> احجز <em>الان</em></h4>
+          </div>
+          <div>
+          <table class="table table-info text-center">
+            <thead>
+                <tr>
+                    <th scope="col">السعر بعد التخفيض</th>
+                    <th scope="col">السعر</th>
+                    <th scope="col">الخدمات</th>
+                </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>400$</td>
+                <td>600$</td>
+                <td>زرع- جاندني ددان كوري</td>
+                </tr>
+                <tr>
+                <td>600$</td>
+                <td>750$</td>
+                <td>زرع- جاندني ددان ايطالي</td>
+                </tr>
+                <tr>
+                    <td>40.000</td>
+                    <td>60.000</td>
+                    <td>حشوة عادي بسيط</td>
+                </tr>
+                <tr>
+                    <td>50.000</td>
+                    <td>70.000</td>
+                    <td>حشوة متوسط</td>
+                </tr>
+                <tr>
+                    <td>60.000</td>
+                    <td>75.000</td>
+                    <td>حشوة عصب</td>
+                </tr>
+                <tr>
+                    <td>60.000</td>
+                    <td>75.000</td>
+                    <td>حشوة تجميلي</td>
+                </tr>
+                <tr>
+                    <td>15.000</td>
+                    <td>40.000</td>
+                    <td>قلع EXO</td>
+                </tr>
+
+                <tr>
+                    <td>100.000</td>
+                    <td>120.000</td>
+                    <td>حشوة عصب 1 قناة</td>
+                </tr>
+                <tr>
+                    <td>120.000</td>
+                    <td>140.000</td>
+                    <td>حشوة عصب 2 قناة</td>
+                </tr>
+                <tr>
+                    <td>140.000</td>
+                    <td>180.000</td>
+                    <td>حشوة عصب 3 قناة</td>
+                </tr>
+                <tr>
+                    <td>160.000</td>
+                    <td>200.000</td>
+
+                    <td>حشوة عصب 4 قناة</td>
+                </tr>
+            </tbody>
+        </table>
+
+
+          </div>
+          </div>
+        </div>
+      </div> -->
+  </section>      
+  
+  <section class="featured-contests">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="section-heading text-center">
+            <h6>معرض الصور</h6>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="item">
+            <div class="thumb">
+              <img src="assets/images/Celebrity-Smile1.jpg" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="item">
+            <div class="thumb">
+              <img src="assets/images/Celebrity-Smile2.jpg" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="item">
+            <div class="thumb">
+              <img src="assets/images/Celebrity-Smile3.jpg" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="item">
+            <div class="thumb">
+              <img src="assets/images/Celebrity-Smile4.jpg" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="item">
+            <div class="thumb">
+              <img src="assets/images/Celebrity-Smile5.jpg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+@endsection
